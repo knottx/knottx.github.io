82e2b2731596d205c995566c190dd2db3ccd5f3a
Knot <knotto.vt@gmail.com>
2024-02-17 22:59:20 +0700

    image loader

--- app/components/project-tile.tsx
+++ app/components/project-tile.tsx
@@ -1,3 +1,4 @@
+import imageLoader from "@/image-loader";
 import Image from "next/image";
 
 export default function ProjectTile(props: ProjectProps) {
@@ -48,6 +49,7 @@ export function ProjectImages(project: Project) {
           height={480}
           alt={props}
           src={"/" + props}
+          loader={imageLoader}
           className={[
             "h-auto",
             "max-w-full",
--- image-loader.ts
+++ image-loader.ts
@@ -1,5 +1,6 @@
 'use client'
+import type { ImageLoaderProps } from 'next/image';
 
-export default function imageLoader(src: string) {
+export default function imageLoader({ src, width, quality }: ImageLoaderProps) {
     return `https://knottx.github.io/${src}`;
 }
